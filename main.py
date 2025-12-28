06aaf9e79d38c72edc8f37f47eb7792e614675d8
DESKTOP-2Q8U04U\Ferna <fernandoacurcio@hotmail.com>
2025-12-28 22:50:09 +0000

    - Added type check - Fixed logic on the spellcheck, now add pontuation in the end of the sentence.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from preprocessing.spellcheck import correct_text
 from nlp.syntax import parse
 from nlp.negation import has_negation
 from nlp.semantics import is_personal
+from nlp.type import detect_type
 
 from core.text_input import get_text_input
 from core.result import empty_result
@@ -9,16 +10,17 @@ from core.result import empty_result
 def main():
     result = empty_result()
 
-    text        = get_text_input()
+    text = get_text_input()
     result["frase_original"] = text
 
-    corrected   = correct_text(text)
+    corrected = correct_text(text)
     result["frase_corrigida"] = corrected
     
-    doc         = parse(corrected)
+    doc = parse(corrected)
 
     result["negacao"] = has_negation(doc)
     result["pessoal"] = is_personal(doc)
+    result["tipo"] = detect_type(result["frase_corrigida"], result["negacao"])
 
     print("\n--- RESULTADO ---")
     for key, value in result.items():
