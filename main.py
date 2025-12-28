ef55fd27f86480798fb7968f029c19a4247c8a7f
DESKTOP-2Q8U04U\Ferna <fernandoacurcio@hotmail.com>
2025-12-28 22:06:58 +0000

    -  Refactor main.py

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-from core.text_input import get_text_input
 from preprocessing.spellcheck import correct_text
 from nlp.syntax import parse
 from nlp.negation import has_negation
 from nlp.semantics import is_personal
 
+from core.text_input import get_text_input
+from core.result import empty_result
+
 def main():
+    result = empty_result()
+
     text        = get_text_input()
+    result["frase_original"] = text
+
     corrected   = correct_text(text)
+    result["frase_corrigida"] = corrected
+    
     doc         = parse(corrected)
-    negation    = has_negation(doc)
-    personal    = is_personal(doc)
+
+    result["negacao"] = has_negation(doc)
+    result["pessoal"] = is_personal(doc)
 
     print("\n--- RESULTADO ---")
-    print("Original :", text)
-    print("Corrigida:", corrected)
-    print("Negação  :", negation)
-    print("Pessoal :", personal)
+    for key, value in result.items():
+        print(f"{key}: {value}")
 
 if __name__ == "__main__":
     main()
